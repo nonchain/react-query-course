2cfef3a53ea4686c464f725a2a19f717de619b6f
progali007 <code.progali@gmail.com>
2023-03-25 14:21:15 +0430

    Define a state to toggle the labels selection and pass it to Issues list section

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,15 +1,26 @@
+import { useEffect, useState } from "react";
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
 export default function Issues() {
+  const [selectedLabels, setSelectedLabels] = useState([]);
+
+  const onSelectLabelHandler = (label) => {
+    setSelectedLabels((prevLabels) =>
+      prevLabels.includes(label)
+        ? prevLabels.filter((currentLabel) => currentLabel !== label)
+        : prevLabels.concat(label)
+    );
+  };
+
   return (
     <div>
       <main>
         <section>
           <h1>Issues</h1>
-          <IssuesList />
+          <IssuesList labels={selectedLabels}/>
         </section>
         <aside>
-          <LabelList />
+          <LabelList selectedLabels={selectedLabels} onSelectLabels={onSelectLabelHandler} />
         </aside>
       </main>
     </div>
