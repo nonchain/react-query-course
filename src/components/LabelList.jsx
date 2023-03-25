5cb9d2b061210f3298c9c4561d5028cb1ed92559
progali007 <code.progali@gmail.com>
2023-03-25 13:56:27 +0430

    Create labels section component

--- src/components/LabelList.jsx
+++ src/components/LabelList.jsx
@@ -1,3 +1,21 @@
+import { useLabelsData } from "../helpers/useLabelsData";
+
 export default function LabelList() {
-  return <h3>Labels</h3>;
+  const labelsQuery = useLabelsData();
+  return (
+    <div>
+      <h3>Labels</h3>
+      {
+        labelsQuery.isLoading ? <p>Loading...</p> : <ul>
+          {
+            labelsQuery?.data?.map(label => <li key={label.id}>
+              <button className={`label ${label.color}`}>
+                {label.name}
+              </button>
+            </li>)
+          }
+        </ul>
+      }
+    </div>
+  );
 }
