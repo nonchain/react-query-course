0626f9206bd36a558cda8cafdce69117739e5ed7
progali007 <code.progali@gmail.com>
2023-03-26 02:05:33 +0430

    Refetch data base on the issues status

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -2,11 +2,12 @@ import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import IssueItem from "./IssueItem";
 
-export default function IssuesList({ labels }) {
-  const issuesQuery = useQuery(["issues", labels], () =>
+export default function IssuesList({ labels, status }) {
+  console.log(status);
+  const issuesQuery = useQuery(["issues", {labels, status}], () =>
     {
       const labelsQueryString = labels?.map(label => `labels[]=${label}`).join("&");
-      return fetch(`/api/issues?${labelsQueryString}`).then((res) => res.json())
+      return fetch(`/api/issues?${labelsQueryString}&status=${status}`).then((res) => res.json())
     }
   );
 
