c51cb8e3ff4124fe0eb5a4bf62332eb3996afa26
progali007 <code.progali@gmail.com>
2023-03-26 02:00:13 +0430

    Implement issues status section

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
+import StatusSelector from "../components/StatusSelector";
 export default function Issues() {
   const [selectedLabels, setSelectedLabels] = useState([]);
+  const [selectedStatus, setSelectedStatus] = useState("");
 
   const onSelectLabelHandler = (label) => {
     setSelectedLabels((prevLabels) =>
@@ -21,6 +23,7 @@ export default function Issues() {
         </section>
         <aside>
           <LabelList selectedLabels={selectedLabels} onSelectLabels={onSelectLabelHandler} />
+          <StatusSelector value={selectedStatus} onChange={({target}) => setSelectedStatus(target?.value)}/>
         </aside>
       </main>
     </div>
