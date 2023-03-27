7ff3cdf606182312f6c2e33d0e7ccb5be624a902
progali007 <code.progali@gmail.com>
2023-03-27 12:40:20 +0430

    Get issue details data

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom";
+import { useIssueData } from "../helpers/useIssueData";
 
 export default function IssueDetails() {
   const { number } = useParams();
+  const issueDetailsQuery = useIssueData(number);
 
-  return <h1>Issue {number}</h1>;
+  return <div className="issue-details">
+    
+  </div>;
 }
