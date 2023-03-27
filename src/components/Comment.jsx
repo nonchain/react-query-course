037c9839c52510261598692173c2b29a16bf500d
progali007 <code.progali@gmail.com>
2023-03-27 13:44:33 +0430

    Create comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useUserData } from "../helpers/useUserData";
+import { relativeDate } from "../helpers/relativeDate";
+
+function Comment({ comment, createdBy, createdDate }) {
+  const userQuery = useUserData(createdBy);
+
+  if (userQuery.isLoading)
+    return (
+      <div className="comment">
+        <div>
+          <div className="comment-header">Loading</div>
+        </div>
+      </div>
+    );
+
+  return (
+    <div className="comment">
+      <img src={userQuery?.data?.profilePictureUrl} alt="user pic" />
+      <div>
+          <div className="comment-header">
+            <span>{userQuery?.data?.name} commented</span>{" "}
+            <span>{relativeDate(createdDate)}</span>
+          </div>
+          <div className="comment-body">{comment}</div>
+        </div>
+    </div>
+  );
+}
+
+export default Comment;
