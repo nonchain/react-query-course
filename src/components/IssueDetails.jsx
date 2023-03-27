9c278ac76add622382b25bff60e867a815578d77
progali007 <code.progali@gmail.com>
2023-03-27 13:44:59 +0430

    Implement comment component

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -1,14 +1,27 @@
 import { useParams } from "react-router-dom";
 import { useIssueData } from "../helpers/useIssueData";
 import IssueHeader from "./IssueHeader";
+import { useIssueComments } from "../helpers/useIssueComments";
+import Comment from "./Comment";
 
 export default function IssueDetails() {
   const { number } = useParams();
   const issueDetailsQuery = useIssueData(number);
+  const commentsQuery = useIssueComments(number);
 
   return <div className="issue-details">
     {issueDetailsQuery.isLoading ? <p>Loading...</p> : <>
       <IssueHeader {...issueDetailsQuery?.data}/>
+
+      <main>
+        <section>
+          {
+            commentsQuery.isLoading ? <p>Loading...</p> :
+            commentsQuery?.data?.map(comment => <Comment key={comment?.id} {...comment}/>)
+          }
+        </section>
+        <aside></aside>
+      </main>
     </>}
   </div>;
 }
