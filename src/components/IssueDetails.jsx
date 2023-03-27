dcdf5bcf765578090ec8d7a1e714e9a403f07fe2
progali007 <code.progali@gmail.com>
2023-03-27 12:46:43 +0430

    Import IssueHeader component

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useIssueData } from "../helpers/useIssueData";
+import IssueHeader from "./IssueHeader";
 
 export default function IssueDetails() {
   const { number } = useParams();
