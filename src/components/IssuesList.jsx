b32d6ddad9ab0ebf4e34f54b6921b03895234d38
progali007 <code.progali@gmail.com>
2023-03-29 20:00:27 +0430

    Create secrch bar and show serch results

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,27 +1,72 @@
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import IssueItem from "./IssueItem";
+import { useState } from "react";
 
 export default function IssuesList({ labels, status }) {
-  console.log(status);
-  const issuesQuery = useQuery(["issues", {labels, status}], () =>
+  const [searchValue, setSearchValue] = useState("");
+
+  const issuesQuery = useQuery(["issues", { labels, status }], () => {
+    const labelsQueryString = labels
+      ?.map((label) => `labels[]=${label}`)
+      .join("&");
+    return fetch(`/api/issues?${labelsQueryString}&status=${status}`).then(
+      (res) => res.json()
+    );
+  });
+  const searchQuery = useQuery(
+    ["issues", "search", searchValue],
+    () =>
+      fetch(`/api/search/issues?q=${searchValue}`).then((res) => res.json()),
     {
-      const labelsQueryString = labels?.map(label => `labels[]=${label}`).join("&");
-      return fetch(`/api/issues?${labelsQueryString}&status=${status}`).then((res) => res.json())
+      enabled: searchValue.length > 0,
     }
   );
 
   return (
     <div>
-      <h2>Issues List</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setSearchValue(event.target.elements.search.value);
+        }}
+      >
+        <label htmlFor="search">Search issue</label>
+        <input
+          type="search"
+          name="search"
+          id="search"
+          placeholder="Search"
+          onChange={(event) => {
+            if (event.target.value.length === 0) {
+              setSearchValue("");
+            }
+          }}
+        />
+      </form>
       {issuesQuery.isLoading ? (
         <p>Loading...</p>
+      ) : searchQuery.fetchStatus === "idle" && searchQuery.isLoading ? (
+        <>
+          <h2>Issues List</h2>
+          <ul className="issues-list">
+            {issuesQuery.data?.map((issue) => (
+              <IssueItem key={issue?.id} issue={issue} />
+            ))}
+          </ul>
+        </>
+      ) : searchQuery.isLoading ? (
+        <p>Loading...</p>
       ) : (
-        <ul className="issues-list">
-          {issuesQuery.data?.map((issue) => (
-            <IssueItem key={issue?.id} issue={issue}/>
-          ))}
-        </ul>
+        <>
+          <h2>Search Result</h2>
+          <span>{searchQuery.data.count} Results</span>
+          <ul className="issues-list">
+            {searchQuery.data?.items?.map((issue) => (
+              <IssueItem key={issue?.id} issue={issue} />
+            ))}
+          </ul>
+        </>
       )}
     </div>
   );
