31938a4af0a8c36ccfd8e431844130041088c33d
progali007 <code.progali@gmail.com>
2023-04-24 20:18:05 +0430

    Build Add issue page

--- src/pages/AddIssue.jsx
+++ src/pages/AddIssue.jsx
@@ -1,3 +1,15 @@
 export default function AddIssue() {
-  return <h2>Add Issue</h2>;
+  return <div className="add-issue">
+    <h2>Add Issue</h2>
+
+    <form>
+      <label htmlFor="title">Title</label>
+      <input type="text" id="title" name="title" placeholder="Title"/>
+
+      <label htmlFor="comment"></label>
+      <textarea id="comment" name="comment" placeholder="Comment"/>
+
+      <button type="submit" disabled={false}>Add Issue</button>
+    </form>
+  </div>;
 }
